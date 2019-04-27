d602da2911caa4409ca9333879cdf87ef0bef985
Jonnas Costa <jonn567@gmail.com>
2019-04-26 22:57:58 -0300

    font file isn't need any more, unless you want to debug

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Doom® Fire Algorithm P5js
-- Another implementation of Doom fire Algorithm
-- PS: Needs a Webserver to work
-- PS: It needs p5 lib from https://p5js.org
+# doom_fire_algorithm_p5
+Another implementation of Doom fire Algorithm
+PS: Needs a Webserver to work
+PS: It needs p5 lib from https://p5js.org
--- scketch_jx.js
+++ scketch_jx.js
@@ -1,11 +1,4 @@
 var grid = undefined;
-//let font,fontsize = 20;
-
-/*function preload() {
-  // Ensure the .ttf or .otf font stored in the assets directory
-  // is loaded before setup() and draw() are called
-  font = loadFont('assets/SourceSansPro-Regular.otf');
-}*/
 
 function setup() {
     var canvas = createCanvas(401,401);
@@ -34,7 +27,5 @@ function draw() {
     const fdecay2 = slider2.value();
     fill(255);
     stroke(0);
-    //text('red', slider.x * 2 + slider.width, 35);
-    //text('green', slider2.x * 2 + slider2.width, 65);
     grid.draw(fdecay1,fdecay2);
 }
